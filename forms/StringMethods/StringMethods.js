66c0793e04233bc42b95b00142bceaf9e15d4f06
Alissa Dolan <alissadolan@alissas-mbp-2.ddns.creighton.edu>
2019-10-03 09:06:30 -0500

    Initial commit

---
+++ forms/StringMethods/StringMethods.js
@@ -0,0 +1,30 @@
+//2a
+let quoteString = 'Failure is simply the opportunity to begin again this time more intelligently.'
+
+//2b
+let upperCaseString = quoteString.toUpperCase()
+console.log(upperCaseString)
+
+//2c
+let authorString = 'Henry Ford'
+
+//2d- look at 
+let completeString = quoteString.concat(authorString)
+console.log(completeString)
+
+//2e
+let secondString ='Talent is cheaper than table salt. What separates the talented individual from the successful one is a lot of hard work. - Stephen King'
+
+//2f
+lowerCaseString = secondString.toLowerCase()
+console.log(lowerCaseString)
+
+
+//2g 
+console.log(secondString.charAt(3));
+ 
+
+//2h 
+let findString = secondString.slice(35, -14);
+console.log(findString)
+
